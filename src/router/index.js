65ee61d9077f1b8a4aa81f0b71a3d5e1fbd7a799
johnnywwy <johnnywwy@gmail.com>
2022-04-10 00:27:04 +0800

    添加/notebooks，/note/:noteId，/trash/:noteId 路由，删除helloworld

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '../components/HelloWorld'
 import Login from '../components/Login'
+import NotebookList from '../components/NotebookList.vue'
+import NoteDetail from '../components/NoteDetail.vue'
+import TashDetail from '../components/TashDetail.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
       path: '/login',
       name: 'Login',
       component: Login
+    }, {
+      path: '/notebooks',
+      name: 'NotebookList',
+      component: NotebookList
+    }, {
+      path: '/note/:noteId',
+      name: 'NoteDetail',
+      component: NoteDetail
+    }, {
+      path: '/trash/:noteId',
+      name: 'TashDetail',
+      component: TashDetail
     }
   ]
 })
