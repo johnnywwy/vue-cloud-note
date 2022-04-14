cc717c8c926a9a3364876b8be12e2abc1361dee0
johnnywwy <johnnywwy@gmail.com>
2022-04-14 15:44:29 +0800

    笔记详情页编辑区结构样式

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -1,9 +1,21 @@
 <template>
   <div id="note" class="detail">
     <NoteSidebar></NoteSidebar>
-    <div id="note-detail">
-      <h1>notebookId : {{ $route.query.notebookId }}</h1>
-      <h1>noteId : {{ $route.query.noteId }}</h1>
+    <div class="note-detail">
+      <div class="note-bar">
+        <span> 创建日期: {{curNote.createdAtFriendly}}</span>
+        <span> 更新日期: {{curNote.updatedAtFriendly}}</span>
+        <span> {{curNote.statusText}}</span>
+        <span class="iconfont icon-delete" @click=""></span>
+        <span class="iconfont icon-fullscreen"></span>
+      </div>
+      <div class="note-title">
+        <input type="text" :value="curNote.title" placeholder="输入标题">
+      </div>
+      <div class="editor">
+        <textarea v-show="true" :value="curNote.content" placeholder="输入内容, 支持 markdown 语法"></textarea>
+        <div class="preview markdown-body" v-html="" v-show="false"></div>
+      </div>
     </div>
   </div>
 
@@ -17,10 +29,15 @@ export default {
   components: {
     NoteSidebar
   },
-  name: 'Login',
   data() {
     return {
-      msg: '笔记详情页'
+      curNote: {
+        title: '我的笔记',
+        content: '我是内容.........',
+        createdAtFriendly: '1天前',
+        updatedAtFriendly: '刚刚',
+        statusText: '已保存'
+      }
     }
   },
   created() {
@@ -34,14 +51,89 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
 #note {
   display: flex;
   align-items: stretch;
   background-color: #fff;
   flex: 1;
 }
-h1{
-  color: cornflowerblue;
+
+.note-detail {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+
+  .note-detail-ct {
+    height: 100%;
+  }
+
+  .note-empty {
+    font-size: 50px;
+    color: #ccc;
+    text-align: center;
+    margin-top: 100px;
+  }
+
+  .note-bar {
+    padding: 4px 20px;
+    border-bottom: 1px solid #eee;
+
+    &:after {
+      content: '';
+      display: block;
+      clear: both;
+    }
+
+    span {
+      font-size: 12px;
+      color: #999;
+      margin-right: 4px;
+    }
+
+    .iconfont {
+      float: right;
+      margin-left: 4px;
+      font-size: 18px;
+      cursor: pointer;
+    }
+
+  }
+
+  .note-title {
+    input, span {
+      display: inline-block;
+      width: 100%;
+      border: none;
+      outline: none;
+      font-size: 18px;
+      padding: 10px 20px;
+    }
+  }
+
+  .editor {
+    height: ~"calc(100% - 70px)";
+    position: relative;
+  }
+
+  textarea, .preview {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    padding: 20px;
+  }
+
+  textarea {
+    border: none;
+    resize: none;
+    outline: none;
+    font-size: 14px;
+    font-family: 'Monaco', courier, monospace;
+  }
+
+  code {
+    color: #f66;
+  }
 }
+
 </style>
