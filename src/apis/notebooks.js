76ee21612459cf973cf219e50e633bdef28d91ad
johnnywwy <johnnywwy@gmail.com>
2022-04-15 22:22:32 +0800

    vuex note模块的完善与 NoteSidebar 的改造

--- src/apis/notebooks.js
+++ src/apis/notebooks.js
@@ -1,6 +1,7 @@
 import request from '../helpers/request'
 import {friendlyDate} from '../helpers/util'
 
+
 const URL = {
   GET: '/notebooks',
   ADD: '/notebooks',
@@ -14,8 +15,9 @@ export default {
       request(URL.GET)
         .then(res => {
           res.data = res.data.sort((notebook1, notebook2) => notebook1.createdAt > notebook2.createdAt ? -1 : 1)
-          res.data.forEach(notebook=>{
-            notebook.friendlyCreatedAt = friendlyDate(notebook.createdAt)
+          res.data.forEach(notebook => {
+            notebook.createdAtFriendly = friendlyDate(notebook.createdAt)
+            notebook.updatedAtFriendly = friendlyDate(notebook.updatedAt)
           })
           resolve(res)
         }).catch(err => {
@@ -24,16 +26,25 @@ export default {
     })
   },
 
-  updateNotebook(notebookId, { title = '' } = { title: '' }) {
-    return request(URL.UPDATE.replace(':id', notebookId), 'PATCH', { title })
+  updateNotebook(notebookId, {title = ''} = {title: ''}) {
+    return request(URL.UPDATE.replace(':id', notebookId), 'PATCH', {title})
   },
 
   deleteNotebook(notebookId) {
     return request(URL.DELETE.replace(':id', notebookId), 'DELETE')
   },
 
-  addNotebook({ title = ''} = { title: ''}) {
-    return request(URL.ADD, 'POST', { title })
+  addNotebook({title = ''} = {title: ''}) {
+    return new Promise((resolve, reject) => {
+      request(URL.ADD, 'POST', {title})
+        .then(res => {
+          res.data.createdAtFriendly = friendlyDate(res.data.createdAt)
+          res.data.updatedAtFriendly = friendlyDate(res.data.updatedAt)
+          resolve(res)
+        }).catch(err => {
+        reject(err)
+      })
+    })
   }
 
 }
--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -31,12 +31,13 @@
 import Auth from '../apis/auth'
 import Notebooks from '../apis/notebooks'
 import {friendlyDate} from '../helpers/util'
+import {mapState, mapActions, mapGetters} from 'vuex'
 
 export default {
   data() {
     return {
-      notebooks: [],
-      msg: '笔记本列表'
+      //notebooks: [],
+      //msg: '笔记本列表'
     }
   },
   created() {
@@ -46,13 +47,18 @@ export default {
           this.$router.push({path: '/login'})
         }
       })
-    Notebooks.getAll()
-      .then(res => {
-        // console.log(res)
-        this.notebooks = res.data
-      })
+    this.$store.dispatch('getNotebooks')
+  },
+  computed: {
+    ...mapGetters(['notebooks'])
   },
   methods: {
+    ...mapActions([
+      'getNotebooks',
+      'addNotebook',
+      'updateNotebook',
+      'deleteNotebook'
+    ]),
     onCreate() {
       this.$prompt('输入笔记本标题', '创建笔记本', {
         confirmButtonText: '确定',
@@ -60,11 +66,7 @@ export default {
         inputPattern: /^.{1,30}$/,
         inputErrorMessage: '标题不能为空，且不超过30个字符'
       }).then(({value}) => {
-        return Notebooks.addNotebook({title: value})
-      }).then(res => {
-        res.data.friendlyCreatedAt = friendlyDate(res.data.createdAt)
-        this.notebooks.unshift(res.data)
-        this.$message.success(res.msg)
+        this.addNotebook({title: value})
       })
     },
     onEdit(notebook) {
@@ -76,12 +78,7 @@ export default {
         inputValue: notebook.title,
         inputErrorMessage: '标题不能为空，且不超过30个字符'
       }).then(({value}) => {
-        console.log('value', value)
-        title = value
-        return Notebooks.updateNotebook(notebook.id, {title})
-      }).then(res => {
-        notebook.title = title
-        this.$message.success(res.msg)
+        this.updateNotebook({notebookId: notebook.id, title: value})
       })
     },
     onDelete(notebook) {
@@ -90,11 +87,7 @@ export default {
         cancelButtonText: '取消',
         type: 'warning'
       }).then(() => {
-        return Notebooks.deleteNotebook(notebook.id)
-      }).then(res => {
-        console.log('res', res)
-        this.notebooks.splice(this.notebooks.indexOf(notebook), 1)
-        this.$message.success(res.msg)
+        this.deleteNotebook({notebookId: notebook.id})
       })
     }
   }
